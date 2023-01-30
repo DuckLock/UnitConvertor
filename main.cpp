da619893e9e5be203c587e76409024dd2e4bb950
DuckLock <90543929+DuckLock@users.noreply.github.com>
2023-01-30 14:00:15 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,20 +7,21 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
-    w.resize(450,240);
+    w.resize(0,0);
     w.setWindowTitle("Unit Convertor");
 
     QVBoxLayout* l = new QVBoxLayout;
 
-    UnitConvertor* calc1 = new UnitConvertor("m","km",0.001);
-    UnitConvertor* calc2 = new UnitConvertor("mm","cm",0.1);
-    UnitConvertor* calc3 = new UnitConvertor("cm","dm",0.1);
-    UnitConvertor* calc4 = new UnitConvertor("dm","m",0.1);
-    UnitConvertor* calc5 = new UnitConvertor("cm","dm",0.1);
 
-    UnitConvertor* calc6 = new UnitConvertor("cm","dm",0.1);
-    UnitConvertor* calc7 = new UnitConvertor("cm","dm",0.1);
-    UnitConvertor* calc8 = new UnitConvertor("cm","dm",0.1);
+    UnitConvertor* calc1 = new UnitConvertor("nm","μm",0.001);
+    UnitConvertor* calc2 = new UnitConvertor("μm","mm",0.001);
+    UnitConvertor* calc3 = new UnitConvertor("mm","cm",0.1);
+    UnitConvertor* calc4 = new UnitConvertor("cm","dm",0.1);
+    UnitConvertor* calc5 = new UnitConvertor("dm","m",0.1);
+    UnitConvertor* calc6 = new UnitConvertor("m","km",0.001);
+
+    UnitConvertor* calc7 = new UnitConvertor("cm/s","m/s",0.01);
+    UnitConvertor* calc8 = new UnitConvertor("m/s","km/h",3.6);
 
     l->addWidget(calc1);
     l->addWidget(calc2);
